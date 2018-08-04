0f302fa8fc738884822bed14a9d5168348f60759
Ian @ home <ian@visdata.co.uk>
2018-08-04 07:11:46 +0100

    Section 5 lecture 34

---
+++ TasteRestaurant/Data/CategoryType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Data
+{
+    public class CategoryType
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Display order")]
+        public int DisplayOrder { get; set; }
+    }
+}
--- TasteRestaurant/Startup.cs
+++ TasteRestaurant/Startup.cs
@@ -27,7 +27,7 @@ namespace TasteRestaurant
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DesktopConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
