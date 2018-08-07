c00359d0dc1ed7909bd497117ca2968d4ecae162
Ian @ home <ian@visdata.co.uk>
2018-08-07 06:08:26 +0100

    Section 6, lecture 50

--- TasteRestaurant/Data/Migrations/20180807045824_AddFoodTypeToDb.Designer.cs
+++ TasteRestaurant/Data/Migrations/20180807045824_AddFoodTypeToDb.Designer.cs
@@ -11,9 +11,10 @@ using TasteRestaurant.Data;
 namespace TasteRestaurant.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180807045824_AddFoodTypeToDb")]
+    partial class AddFoodTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
