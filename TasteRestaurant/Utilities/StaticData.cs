b2ba33a26a155bbda678759e312919dbc4f7f260
Ian @ home <ian@visdata.co.uk>
2018-08-10 06:15:25 +0100

    Section 7, lecture 63

---
+++ TasteRestaurant/Utilities/StaticData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasteRestaurant.Utilities
+{
+    public class StaticData
+    {
+        public const string DefaultFoodImage = "default_food.png";
+        public const string DefaultImageExtension = ".png";
+    }
+}
